119cafd83c88f7635ef47c4a141bd263a6d066ff
SilverCat <datpd0507@gmail.com>
2022-09-18 14:25:21 +0700

    init project

---
+++ src/main/java/com/seisilver/hololive/clip/api/HololiveClipApiApplication.java
@@ -0,0 +1,13 @@
+package com.seisilver.hololive.clip.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HololiveClipApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HololiveClipApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seisilver/hololive/clip/api/HololiveClipApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seisilver.hololive.clip.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HololiveClipApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
